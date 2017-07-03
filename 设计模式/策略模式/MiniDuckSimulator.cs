f0571c22ed38bcc495f1cec2819e171ed498404d
余作想 <963526232@qq.com>
2017-07-03 14:18:34 +0800

    111

--- 设计模式/策略模式/MiniDuckSimulator.cs
+++ 设计模式/策略模式/MiniDuckSimulator.cs
@@ -9,7 +9,7 @@ namespace 策略模式
     class MiniDuckSimulator
     {
         /// <summary>
-        /// 
+        /// 策略模式：定义了算法族，分别封装起来，让它们之间可以互相替换，此模式让算法的变化独立于使用算法的客户
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
